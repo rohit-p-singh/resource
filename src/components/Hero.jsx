f7e8446f096475f48d9ec1a950368494a94be317
rohit-p-singh <rohitpsingh2035@gmail.com>
2026-04-19 16:32:55 +0530

    changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
           <h1>Explore Your Campus Resources</h1>
           <p>Access notices, notes and jobs in one place.</p>
           <button className="start">Get Started</button>
-        </div>
+          </div>
 
       </div>
     );
